6df924479a83d9a71558e108f6494c43710ca7f4
alaicadm <marinoalaica@gmail.com>
2022-01-26 13:47:06 +0800

    revert to codebehind, crud added

--- PhoneBook/Views/MainWindow.xaml.cs
+++ PhoneBook/Views/MainWindow.xaml.cs
@@ -14,6 +14,9 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Data.SqlClient;
+using System.Data;
+using System.ComponentModel;
 
 namespace PhoneBook
 {
@@ -22,12 +25,130 @@ namespace PhoneBook
     /// </summary>
     public partial class MainWindow : Window
     {
+        //private SqlConnection conn;
+        private string connectionString;
+        private SqlCommand cmd_fill;
+        public IList<Contact> _contactsList = new List<Contact>();
         public MainWindow()
         {
             InitializeComponent();
             DataContext = new MainWindowVM();
 
+            connectionString = @"Data Source=localhost; Initial Catalog = Phonebook; Integrated Security=True";
+            //SqlConnection conn = new SqlConnection(connectionString);
+
+        }
+
+        public void refreshList(object sender, EventArgs e)
+        {
+            try
+            {
+                ICollectionView view = CollectionViewSource.GetDefaultView(UserList.ItemsSource);
+                view.Refresh();
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            finally
+            {
+                MessageBox.Show("refreshed");
+            }
             
+            
+            /*SqlConnection conn = new SqlConnection(connectionString);
+            conn.Open();
+            cmd_fill = new SqlCommand("SELECT * FROM Contacts", conn);
+            SqlDataAdapter adapter = new SqlDataAdapter(cmd_fill);
+            DataSet ds = new DataSet();
+            adapter.Fill(ds, "Contacts");
+
+            try
+            {
+                if (_contactsList != null)
+                {
+                    foreach (DataRow dataRow in ds.Tables[0].Rows)
+                    {
+                        _contactsList.Add(new Contact
+                        {
+                            UserId = (int)dataRow["UserId"],
+                            FirstName = dataRow["FirstName"].ToString(),
+                            MiddleName = dataRow["MiddleName"].ToString(),
+                            LastName = dataRow["LastName"].ToString(),
+                            Gender = dataRow["Gender"].ToString(),
+                            Mobile = dataRow["Mobile"].ToString()
+
+                        });
+                    }
+
+                }
+                MessageBox.Show("refreshed");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            finally
+            {
+                adapter.Dispose();
+                conn.Close();
+                conn.Dispose();
+            }*/
+
+        }
+
+        public bool fieldChecker(object sender, EventArgs e)
+        {
+            bool res = true;
+            if (string.IsNullOrEmpty(fname.Text)) { res = false; }
+            if (string.IsNullOrEmpty(mname.Text)) { res = false; }
+            if (string.IsNullOrEmpty(lname.Text)) { res = false; }
+            if (string.IsNullOrEmpty(gender.Text)) { res = false; }
+            if (string.IsNullOrEmpty(mobile.Text)) { res = false; }
+            return res;
+
+        }
+        public void addContact(object sender, EventArgs e)
+        {
+            bool check = fieldChecker(sender, e);
+
+            if (check == true)
+            {
+
+                SqlConnection conn = new SqlConnection(connectionString);
+                SqlCommand cmd = conn.CreateCommand();
+                cmd.CommandText = "INSERT INTO " +
+                                    "Contacts (FirstName, MiddleName, LastName, Gender, Mobile)" +
+                                    "VALUES (@FirstName, @MiddleName, @LastName, @Gender, @Mobile)";
+                cmd.Parameters.AddWithValue("@FirstName", fname.Text);
+                cmd.Parameters.AddWithValue("@MiddleName", mname.Text);
+                cmd.Parameters.AddWithValue("@LastName", lname.Text);
+                cmd.Parameters.AddWithValue("@Gender", gender.Text);
+                cmd.Parameters.AddWithValue("@Mobile", mobile.Text);
+
+                try
+                {
+                    conn.Open();
+                    cmd.ExecuteNonQuery();
+                    MessageBox.Show("A contact has been added! Refresh table.", "Phonebook", MessageBoxButton.OK, MessageBoxImage.Information, MessageBoxResult.OK);
+
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Data not added");
+                }
+                finally
+                {
+                    conn.Close();
+
+                }
+            }
+            else
+            {
+                MessageBox.Show("An empty field was recognized, check your inputs!");
+            }
+                
+               
         }
 
         public void refreshOnClick(object sender, EventArgs e)
@@ -39,30 +160,74 @@ namespace PhoneBook
             gender.SelectedIndex = -1;
             mobile.Clear();
             
+        }
+
+        public void updateContact(object sender, EventArgs e)
+        {
+            SqlConnection conn = new SqlConnection(connectionString);
+            SqlCommand cmd = conn.CreateCommand();
+            var selectedRow = UserList.SelectedItem;
+            Contact req = selectedRow as Contact;
+            long id = req.UserId;
+            cmd.CommandText = $"UPDATE Contacts SET firstName=@FirstName, middleName=@MiddleName, lastName=@LastName, gender=@Gender, mobile=@Mobile WHERE userId= {id} ";
+            cmd.Parameters.AddWithValue("@FirstName",fname.Text);
+            cmd.Parameters.AddWithValue("@MiddleName", mname.Text);
+            cmd.Parameters.AddWithValue("@LastName", lname.Text);
+            cmd.Parameters.AddWithValue("@Gender", gender.Text);
+            cmd.Parameters.AddWithValue("@Mobile", mobile.Text);
+
+            try
+            {
+                conn.Open();
+                cmd.ExecuteNonQuery();
+                MessageBox.Show("A contact has been updated! Refresh table.", "Phonebook", MessageBoxButton.OK, MessageBoxImage.Information, MessageBoxResult.OK);
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Data not updated");
+            }
+            finally
+            {
+                conn.Close();
+
+            }
 
         }
 
-
-
-
-
-        public void addOnClick(object sender, EventArgs e)
+        public void deleteContact(object sender, EventArgs e)
         {
 
+            SqlConnection conn = new SqlConnection(connectionString);
+            SqlCommand cmd = conn.CreateCommand();
+            var selectedRow = UserList.SelectedItem;
+            Contact req = selectedRow as Contact;
+            long id = req.UserId;
+            cmd.CommandText = $"DELETE FROM Contacts WHERE userId = {id}";
+           
+            try
+            {
+                conn.Open();
+                cmd.ExecuteNonQuery();
+                MessageBox.Show("A contact has been removed! Refresh table.", "Phonebook", MessageBoxButton.OK, MessageBoxImage.Information, MessageBoxResult.OK);
 
-            Contact contact = new Contact();
-            contact.FirstName = fname.Text;
-            contact.MiddleName = mname.Text;
-            contact.LastName = lname.Text;
-            contact.Gender = gender.Text;
-            contact.Mobile = mobile.Text;
-            
-            
-            MessageBox.Show("A contact has been added!", "Phonebook", MessageBoxButton.OK, MessageBoxImage.Information, MessageBoxResult.OK);
-            
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Data on ID: {id} was not deleted.", "Phonebook", MessageBoxButton.OK, MessageBoxImage.Warning, MessageBoxResult.OK);
+            }
+            finally
+            {
+                conn.Close();
+            }
+
 
 
         }
 
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
     }
 }
